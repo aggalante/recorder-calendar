fe328ffbb94255d53527c7f24d89d2fc21fd9559
Adriano Galante <adriano.galante@talkdesk.com>
2021-01-30 15:23:31 +0000

    Recorder calendar project code (initial commit)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Recorder from '../Recorder';
+import Calendar from '../Calendar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Recorder />
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
